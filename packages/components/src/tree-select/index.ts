04f6786cda42529a1723a5857b1f538a7408dcf8
uxuip <uxuip@outlook.com>
2023-10-23 15:50:27 +0800

    feat(tree-select): init

---
+++ packages/components/src/tree-select/index.ts
@@ -0,0 +1,29 @@
+import { TreeSelect as ATreeSelect } from '@arco-design/web-vue'
+import { connect, mapProps } from '@formily/vue'
+import type { TreeNodeData } from '@arco-design/web-vue'
+import type { Field, FieldDataSource } from '@formily/core'
+
+export const TreeSelect = connect(
+  ATreeSelect,
+  mapProps(
+    {
+      value: 'modelValue',
+      readOnly: 'readonly',
+    },
+    (props, field) => {
+      function transformData(data: (FieldDataSource & TreeNodeData)): TreeNodeData[] {
+        return data.map((item) => {
+          return {
+            ...item,
+            children: item.children && transformData(item.children),
+            title: item.label,
+            key: item.value,
+          }
+        })
+      }
+      return {
+        data: transformData((field as Field).dataSource),
+      }
+    },
+  ),
+)
