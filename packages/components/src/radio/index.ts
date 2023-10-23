46a7bf09126d657af9b455c404fcafcd325fb04c
uxuip <uxuip@outlook.com>
2023-10-23 17:47:50 +0800

    feat(radio): init

---
+++ packages/components/src/radio/index.ts
@@ -0,0 +1,30 @@
+import { Radio as ARadio } from '@arco-design/web-vue'
+import { connect, mapProps } from '@formily/vue'
+import { defineComponent, h } from 'vue'
+import { typeable } from '../__builtins__/shared/maps'
+import { composeExport } from '../__builtins__/shared'
+
+const _Radio = connect(
+  defineComponent({
+    setup(_, { attrs }) {
+      return () => h(ARadio, { attrs }, { default: () => attrs.title })
+    },
+  }),
+  mapProps({
+    ...typeable,
+    title: true,
+  }),
+)
+
+export const RadioGroup = connect(
+  ARadio.Group,
+  mapProps({
+    value: 'modelValue',
+    readOnly: 'readonly',
+    dataSource: 'options',
+  }),
+)
+
+export const Radio = composeExport(_Radio, {
+  Group: RadioGroup,
+})
